7c267c77ee3f1f196ba91114d6b6ab76c185697d
HybridDog <adresse_test@t-online.de>
2014-04-05 17:48:29 +0200

    do not place a node if it is already there and change a bit other things

--- init.lua
+++ init.lua
@@ -1,18 +1,24 @@
 minetest.register_node("killer:killer", {
-        tiles = {"killer_killer.png"},
-        groups = {crumbly=1},
-        description = "[!] Killer [!]",
-        inventory_image = minetest.inventorycube("killer_killer.png", "killer_killer.png", "killer_killer.png"),
-        light_source = 14,
+		tiles = {"killer_killer.png"},
+		groups = {crumbly=1},
+		description = "[!] Killer [!]",
+		light_source = 14,
 })
 
 minetest.register_abm({
-        nodenames = {"killer:killer"},
-        chance = 5,
-        interval = 5,
-        action = function(pos, node, active_object_count, active_object_count_wider)
-                minetest.set_node({x=pos.x+(math.random(1, 3) * math.random(-1, 1)), y=pos.y+(math.random(1, 3) * math.random(-1, 1)), z=pos.z+(math.random(1, 3)*math.random(-1, 1))}, {name="killer:killer"})
-        end,
+		nodenames = {"killer:killer"},
+		chance = 5,
+		interval = 5,
+		action = function(pos)
+			local p = {
+				x=pos.x+(math.random(1, 3) * math.random(-1, 1)),
+				y=pos.y+(math.random(1, 3) * math.random(-1, 1)),
+				z=pos.z+(math.random(1, 3) * math.random(-1, 1))
+			}
+			if minetest.get_node(p).name ~= "killer:killer" then
+				minetest.set_node(p, {name="killer:killer"})
+			end
+		end,
 })
 
 minetest.register_craft({
@@ -21,5 +27,5 @@ minetest.register_craft({
 		{"default:gold_ingot", "default:steel_ingot", "default:gold_ingot"},
 		{"default:steel_ingot", "", "default:steel_ingot"},
 		{"default:gold_ingot", "default:steel_ingot", "default:gold_ingot"},
-		},
+	},
 })
